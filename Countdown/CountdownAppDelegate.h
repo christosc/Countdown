f41c8f60e4a5bdbaf872f26f6acc8caa0a441c6f
Christos Chryssochoidis <C.Chryssochoidis@gmail.com>
2011-04-19 04:14:21 +0300

    First commit.

--- Countdown/CountdownAppDelegate.h
+++ Countdown/CountdownAppDelegate.h
@@ -1,16 +1,34 @@
 //
-//  CountdownAppDelegate.h
-//  Countdown
+//  TimerAppDelegate.h
+//  Timer
 //
-//  Created by Christos Chryssochoidis on 4/19/11.
+//  Created by Christos Chryssochoidis on 4/18/11.
 //  Copyright 2011 University of Athens. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#define DEFAULT_TIMEOUT 60  //ἀριθμὸς λεπτῶν τῆς ὥρας δι' ἐκπνοὴν τῆς ἀντιστρόφου 
+                            // μετρήσεως
 
 @interface CountdownAppDelegate : NSObject <NSApplicationDelegate> {
 @private
     NSWindow *window;
+    NSView *view;
+    NSStatusItem *statusItem;
+    NSButton *startButton;
+    NSButton *stopButton;
+    NSTextField *timeoutLabel;
+    NSTextField *timeoutField;  // λεπτὰ προθεσμίας
+    NSTextField *remainingTimeLabel;
+    NSTextField *remainingTimeField;  // λεπτὰ ἐναπομένοντος χρόνου
+    NSTimer *updateTimer; // χρονιστὴς δι' ἀνανέωσιν ἀνὰ λεπτὸν τῆς 
+                       // ἐνδείξεως
+    NSTimer *timeoutTimer; // χρονιστὴς διὰ παῦσιν τοῦ secTimer ἐπὶ τῇ ἐκπνοῇ 
+                           // τῆς προθεσμίας
+//    int timeout;  // ἀριθμὸς λεπτῶν τῆς ὥρας δι' ἐκπνοὴν τῆς ἀντιστρόφου 
+//                     // μετρήσεως
+    int remainingTime; // ἐναπομένοντα λεπτά δι' ἐκπνοὴν τῆς ἀντ.μετρήσεως
+    int timeout;
 }
 
 @property (assign) IBOutlet NSWindow *window;
--- Countdown/CountdownAppDelegate.m
+++ Countdown/CountdownAppDelegate.m
@@ -1,10 +1,14 @@
 //
-//  CountdownAppDelegate.m
-//  Countdown
+//  TimerAppDelegate.m
+//  Timer
 //
-//  Created by Christos Chryssochoidis on 4/19/11.
+//  Created by Christos Chryssochoidis on 4/18/11.
 //  Copyright 2011 University of Athens. All rights reserved.
 //
+
+// Χρειάζεται νὰ ἔχω δύο χρονιστὰς, ἕνα ἐπαναλαμβανόμενον διὰ τὴν κατὰ 
+// δευτερόλεπτον (ἢ λεπτὸν) ἀνανέωσιν τῆς ἐνδείξεως ἐν τῇ ράβδῳ καταστάσεως, 
+// καὶ ἕνα πυροδοτούμενον ἐφάπαξ γιὰ νὰ παύσῃ τὸν προηγούμενον χρονιστήν.
 
 #import "CountdownAppDelegate.h"
 
@@ -14,7 +18,166 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    //--------------------  ΚΟΥΜΠΙΑ ---------------------------------
+    
+    //--------------------  Κουμπὶ ἐνάρξεως -------------------------
+    
+    view = [[NSView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [window setContentView:view];
+    [view release];
+    startButton = [[NSButton alloc] initWithFrame:CGRectMake(10, 10, 60, 50)];
+    [startButton setTitle:@"Ἄρχισον"];
+    [startButton setTarget:self];
+    [startButton setAction:@selector(startCountdown:)];
+    [view addSubview:startButton];
+    [startButton release];
+    
+    
+    //-------------------- Κουμπὶ παύσεως ---------------------------
+    
+    stopButton = [[NSButton alloc] initWithFrame:CGRectMake(90, 10, 60, 50)];
+    [stopButton setTitle:@"Παῦσον"];
+    [stopButton setTarget:self];
+    [stopButton setAction:@selector(stopCountdown:)];
+    [view addSubview:stopButton];
+    [stopButton release];
+    
+    
+    //--------------------- ΠΕΔΙΑ—ΕΠΙΓΡΑΦΑΙ -------------------------
+    
+    //--------------------- Προθεσμία -------------------------------
+    
+    timeoutLabel = [[NSTextField alloc] 
+                    initWithFrame:CGRectMake(10, 100, 90, 20)];
+    [timeoutLabel setStringValue:@"Προθεσμία:"];
+    [timeoutLabel setEditable:NO];
+    [timeoutLabel setDrawsBackground:NO];
+    [timeoutLabel setBezeled:NO];
+    [view addSubview:timeoutLabel];
+    [timeoutLabel release]; 
+    [timeoutLabel setAlignment:NSLeftTextAlignment];
+    
+    
+    
+    
+    timeoutField = [[NSTextField alloc] 
+                        initWithFrame:CGRectMake(100, 100, 40, 20)];
+    [timeoutField setStringValue:[[NSNumber numberWithInt:DEFAULT_TIMEOUT] 
+                                    stringValue]];
+    [view addSubview:timeoutField];
+    [timeoutField release];
+    [timeoutField setAlignment:NSCenterTextAlignment];
+    
+    
+    NSTextField *ending = [[NSTextField alloc] 
+                           initWithFrame:CGRectMake(140, 100, 50, 20)];
+    [ending setStringValue:@"λεπτά"];
+    [ending setEditable:NO];
+    [ending setDrawsBackground:NO];
+    [ending setBezeled:NO];
+    [view addSubview:ending];
+    [ending release]; 
+    [ending setAlignment:NSLeftTextAlignment];
+    
+    
+    //-------------------- Ἐναπομένων χρόνος ----------------------
+    
+    remainingTimeLabel = [[NSTextField alloc] 
+                    initWithFrame:CGRectMake(10, 70, 90, 20)];
+    [remainingTimeLabel setStringValue:@"ἐναπομένων:"];
+    [remainingTimeLabel setEditable:NO];
+    [remainingTimeLabel setDrawsBackground:NO];
+    [remainingTimeLabel setBezeled:NO];
+    [view addSubview:remainingTimeLabel];
+    [remainingTimeLabel release]; 
+    [remainingTimeLabel setAlignment:NSLeftTextAlignment];
+    
+    
+    
+    
+    remainingTimeField = [[NSTextField alloc] 
+                    initWithFrame:CGRectMake(100, 70, 40, 20)];
+//  [remainingTimeField setStringValue:[[NSNumber numberWithInt:DEFAULT_TIMEOUT] 
+//                                  stringValue]];
+    [view addSubview:remainingTimeField];
+    [remainingTimeField release];
+    [remainingTimeField setAlignment:NSCenterTextAlignment];
+    [remainingTimeField setEditable:NO];
+    
+    
+    ending = [[NSTextField alloc] 
+                           initWithFrame:CGRectMake(140, 70, 50, 20)];
+    [ending setStringValue:@"λεπτά"];
+    [ending setEditable:NO];
+    [ending setDrawsBackground:NO];
+    [ending setBezeled:NO];
+    [view addSubview:ending];
+    [ending release]; 
+    [ending setAlignment:NSLeftTextAlignment];
 }
 
+- (void)startCountdown:(id)sender
+{
+    NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
+    statusItem = [statusBar statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem retain];
+    [statusItem setEnabled:YES];
+    [statusItem setToolTip:@"Ἐναπομένοντα λεπτά"];
+    timeout = (int)[timeoutField integerValue];
+    [timeoutField setEnabled:NO];
+    [timeoutField setEditable:NO];
+    [timeoutField setBezeled:NO];
+    [timeoutField setDrawsBackground:NO];
+    remainingTime = timeout;
+    
+    NSString *initialStringValue = [[NSNumber numberWithInt:remainingTime] 
+                                    stringValue];
+    [statusItem setTitle:initialStringValue];
+    [remainingTimeField setStringValue:initialStringValue];
+
+    updateTimer = [NSTimer scheduledTimerWithTimeInterval:60 // κάθε 60δευτ.
+                                                   target:self 
+                                        selector:@selector(updateStatusItem:) 
+                                                 userInfo:nil
+                                                  repeats:YES];
+    
+    timeoutTimer = [NSTimer scheduledTimerWithTimeInterval:timeout * 60
+                                                    target:self
+                                            selector:@selector(stopCountdown:)
+                                                  userInfo:nil
+                                                   repeats:NO];
+    
+    
+    
+}
+
+- (void)updateStatusItem:(id)sender
+{
+    NSTimer *timer = (NSTimer*)sender;
+    remainingTime = (remainingTime*60 - [timer timeInterval]) / 60;
+    NSString *newStringValue = [[NSNumber numberWithInt:remainingTime] 
+                                     stringValue];
+    [statusItem setTitle:newStringValue];
+    [remainingTimeField setStringValue:newStringValue];
+}
+
+- (void)stopCountdown:(id)sender
+{
+    [updateTimer invalidate];
+    NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
+    [statusBar removeStatusItem:statusItem];
+    [statusItem release];
+    [remainingTimeField setStringValue:@"0"];
+    [timeoutField setEditable:YES];
+    [timeoutField setBezeled:YES];
+    [timeoutField setDrawsBackground:YES];
+    
+}
+
+//- (void)applicationWillTerminate:(NSNotification *)notification
+//{
+//    if (<#condition#>) {
+//        <#statements#>
+//    }
+//}
 @end
